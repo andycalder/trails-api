99ac69711f74e0351cca5992d0dcd5d1c4f5b095
Andrew Calder <andrew.calder92@gmail.com>
2021-08-29 21:50:42 +1000

    Generate controller and route

---
+++ app/controllers/api/v1/trails_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::TrailsController < ApplicationController
+	def index
+    @trails = Trail.all
+    render json: trails_json
+  end
+
+  private
+
+  def trails_json
+    trails_array = @trails.map do |trail|
+      {
+        name: trail.name,
+        difficulty: trail.difficulty,
+        type: trail.category,
+        zone: trail.zone,
+        start: [trail.start_lng, trail.start_lat]
+      }
+    end
+
+    { trails: trails_array }
+  end
+end
