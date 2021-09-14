420198b74bf3ade5f0208e1f99b8de0af43cc0f5
Andrew Calder <andrew.calder92@gmail.com>
2021-09-14 21:10:00 +1000

    Enable Sidekiq dashboard

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,13 @@ module TrailsApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.active_job.queue_adapter = :sidekiq
+
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
   end
 end
